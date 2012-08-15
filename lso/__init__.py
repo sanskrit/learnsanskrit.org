2304b7c915b5b7e7171ea80b1dc4f76f9a8572b1
Arun Prasad <akprasad@stanford.edu>
2012-08-14 22:48:13 -0700

    Add basic CSS with flask-assets

--- lso/__init__.py
+++ lso/__init__.py
@@ -1,8 +1,27 @@
 from flask import Flask
+from flask.ext.assets import Bundle, Environment
 
 app = Flask(__name__)
 app.config.from_object('config.development')
 
+# Assets
+# ------
+assets = Environment(app)
+assets.url = '/static'
+
+# Force automatic updates in development
+if app.debug:
+    assets.manifest = None
+    assets.cache = False
+    assets.updater = False
+
+less_files = ['css/%s.less' % x
+              for x in 'base'.split()]
+less = Bundle(*less_files, filters='less', output='gen/style.css', debug=False)
+assets.register('all-css', less)
+
+# Views
+# -----
 import lso.common.views
 
 from guide import guide
