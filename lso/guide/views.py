73a15f04c8504cae4125fa6558bf328d0a1e491e
Arun Prasad <akprasad@stanford.edu>
2012-08-15 22:43:51 -0700

    Add basic table of contents

--- lso/guide/views.py
+++ lso/guide/views.py
@@ -1,7 +1,30 @@
 from flask import render_template
 
 from . import guide
-                  
+from .models import Lesson
+
 @guide.route('/')
 def index():
-    return render_template('guide/index.html')
+    roots = Lesson.query.filter(Lesson.parent_id==None).all()
+    guide_root = next(x for x in roots if x.slug == '')
+
+    lessons = guide_root.mp.query_descendants().all()
+    unit = None
+    units = []
+    for L in lessons:
+        if L.mp_depth == 1:
+            unit = L
+            units.append(unit)
+            unit.children = []
+        else:
+            unit.children.append(L)
+
+    return render_template('guide/index.html', units=units)
+
+@guide.route('/<unit>')
+def unit(unit):
+    pass
+
+@guide.route('/<unit>/<lesson>')
+def lesson(unit, lesson):
+    pass
