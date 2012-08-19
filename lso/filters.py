a6680c06c136be8f7129a5c33b4023666e6dda9a
Arun Prasad <akprasad@stanford.edu>
2012-08-19 11:19:35 -0700

    Add transliteration filters
    Add filters 'sa1' and 'sa2' for transliterating to Devanagari and IAST,
    respectively. Also, add shorthand filters 'd' and 'i', which are mainly
    used within 'guide'.

---
+++ lso/filters.py
@@ -0,0 +1,22 @@
+from jinja2 import Markup
+from sanskrit.letters import sanscript
+
+from lso import app
+
+@app.template_filter()
+def sa1(text, _from=sanscript.SLP1, _to=sanscript.DEVANAGARI, tag='span'):
+    """Return a primary Sanskrit string in the given tag."""
+    sans = sanscript.transliterate(text or '', _from, _to)
+    if tag:
+        return Markup('<%s class="sa1">%s</%s>' % (tag, sans, tag))
+    else:
+        return Markup(sans)
+
+@app.template_filter()
+def sa2(text, _from=sanscript.SLP1, _to=sanscript.IAST, tag='span'):
+    """Return a secondary Sanskrit string in the given tag."""
+    sans = sanscript.transliterate(text or '', _from, _to)
+    if tag:
+        return Markup('<%s lang="sa" class="sa2">%s</%s>' % (tag, sans, tag))
+    else:
+        return Markup(sans)
--- lso/guide/filters.py
+++ lso/guide/filters.py
@@ -1,8 +1,19 @@
+from sanskrit.letters import sanscript
+
 from lso import app
+from lso.filters import sa1, sa2
 
 @app.context_processor
 def inject_notes():
     return {'notes':[]}
+
+@app.template_filter()
+def d(text, tag='span'):
+    return sa1(text, sanscript.HARVARD_KYOTO, tag=tag)
+
+@app.template_filter()
+def i(text, tag='span'):
+    return sa2(text, sanscript.HARVARD_KYOTO, tag=tag)
 
 @app.template_filter()
 def foot(text, notes):
