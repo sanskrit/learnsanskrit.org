63a39b47be12968286a1d90b82a7c598293b3084
Arun Prasad <akprasad@stanford.edu>
2012-09-02 16:27:14 -0700

    Add 'tabs' plugin
    Add a small plugin to show different tabs. In the future, this might
    move from raw jQuery to Backbone.

--- lso/static/js/jquery-plugins.js
+++ lso/static/js/jquery-plugins.js
@@ -2,7 +2,7 @@
  * Basic spotlight
  */
 
-$.fn.spotlightIn = function(duration, easing, callback) {
+$.fn.spotlightOn = function(duration, easing, callback) {
     var $this = $(this),
         $spotlight = $('#spotlight');
     if (!$spotlight.length) {
@@ -14,7 +14,7 @@ $.fn.spotlightIn = function(duration, easing, callback) {
     return this;
 };
 
-$.fn.spotlightOut = function(duration, easing, callback) {
+$.fn.spotlightOff = function(duration, easing, callback) {
     var $this = $(this),
         $spotlight = $('#spotlight');
 
@@ -30,9 +30,44 @@ $.fn.spotlightOut = function(duration, easing, callback) {
 
 $.fn.spotlight = function(duration, easing) {
     var $this = $(this);
-    $this.spotlightIn(duration, easing, function() {
+    $this.spotlightOn(duration, easing, function() {
         $(window, $this).one('click', function() {
-            $this.spotlightOut(duration, easing);
+            $this.spotlightOff(duration, easing);
+        });
+    });
+};
+
+$.fn.tabs = function() {
+    var $this = $(this),
+        $panes = $('> section, > div', $this).hide(),
+        $tabs = $('ul.tab-links li', $this);
+
+    // If the list of tabs is not defined, cerate a new list by inspecting
+    // panes and their headers.
+    if (!$tabs.length) {
+        $links = $('<ul class="tab-links" />');
+        $panes.each(function() {
+            var $pane = $(this),
+                id = $pane.attr('id'),
+                text = $(':header', $pane).first().hide().text(),
+                $li = $('<li><a href="#' + id + '">' + text + '</a></li>');
+                $li.appendTo($links);
+        });
+        $links.prependTo($this);
+        $tabs = $('li', $links);
+    }
+
+    $panes.first().show();
+    $tabs.first().addClass('active');
+
+    $links.on('click', 'a', function(e) {
+        e.preventDefault();
+        var id = this.hash;
+        $tabs.removeClass('active');
+        $(this).closest('li').addClass('active');
+
+        $.when($panes.fadeOut(200)).then(function() {
+            $(id).fadeIn(200);
         });
     });
 };
