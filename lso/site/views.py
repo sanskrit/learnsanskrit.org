35c1d9d482a99ccaecb1fe708938b9c0565382d3
Arun Prasad <akprasad@stanford.edu>
2012-09-05 11:52:33 -0700

    Create four pages in 'site'
    These are:
      - about
      - contact
      - index
      - resources

--- lso/site/views.py
+++ lso/site/views.py
@@ -1,3 +1,19 @@
 from flask import render_template
 
 from . import site
+
+@site.route('/about')
+def about():
+    return render_template('site/about.html')
+
+@site.route('/contact')
+def contact():
+    return render_template('site/contact.html')
+
+@site.route('/site')
+def index():
+    return render_template('site/index.html')
+
+@site.route('/resources')
+def resources():
+    return render_template('site/resources.html')
