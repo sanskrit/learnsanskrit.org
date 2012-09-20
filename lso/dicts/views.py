783acda539eae2b473baf78b356dffe1325412c8
Arun Prasad <akprasad@stanford.edu>
2012-09-20 15:50:55 -0600

    Add first version of MW dictionary
    Users can query for single or multiple terms. Results are displayed on
    a new page as unstyled and unmodified XML.

--- lso/dicts/views.py
+++ lso/dicts/views.py
@@ -1,7 +1,10 @@
-from flask import render_template
+from collections import OrderedDict
+from flask import render_template, request
+from sanskrit.letters import sanscript as S
 
 from lso.forms import QueryForm
 from . import dicts
+from .models import MonierEntry
 
 @dicts.route('/')
 def index():
@@ -9,5 +12,35 @@ def index():
 
 @dicts.route('/mw/')
 def mw():
-    form = QueryForm()
-    return render_template('dicts/mw/index.html', form=form)
+    form = QueryForm(request.args, csrf_enabled=False)
+
+    if form.validate():
+        q = form.data['q']
+        from_script = form.data['from_script']
+        to_script = form.data['to_script']
+
+        slp_query = S.transliterate(q, from_script, S.SLP1)
+        results = mw_results(slp_query)
+
+        return render_template('dicts/mw/index.html', form=form,
+                                                      to_script=to_script,
+                                                      results=results)
+    else:
+        return render_template('dicts/mw/index.html', form=form)
+
+def mw_results(q):
+    """For the given (SLP1) query string, returned an OrderedDict of results
+    from the Monier-Williams dictionary.
+    """
+    q_list = q.replace('+', ' ').replace(',', ' ').split()
+
+    entries = MonierEntry.query.filter(MonierEntry.entry.in_(q_list))
+    entries = entries.order_by('id')
+
+    results = OrderedDict()
+    for entry in q_list:
+        results[entry] = []
+
+    for e in entries:
+        results[e.entry].append(e.data)
+    return results
