2c22e1d95998d6840d3b3be8ef8fb167df90eaa1
Arun Prasad <akprasad@stanford.edu>
2012-09-19 18:24:48 -0600

    Add unit tests for the contact form
    These tests check the following cases:
      - subject, email, message
      - subject, invalid email, message
      - subject, message
      - subject
      - message

--- lso_tests.py
+++ lso_tests.py
@@ -8,6 +8,7 @@ class LSOTestCase(unittest.TestCase):
         """Before each test, create a new database."""
         self.db_file, lso.app.config['DATABASE'] = tempfile.mkstemp()
         lso.app.config['TESTING'] = True
+        lso.app.config['CSRF_ENABLED'] = False
         self.app = lso.app.test_client()
         lso.database.init()
 
@@ -15,6 +16,54 @@ class LSOTestCase(unittest.TestCase):
         os.close(self.db_file)
         os.unlink(lso.app.config['DATABASE'])
 
+    def contact(self, data):
+        """POST data to the contact form."""
+        return self.app.post('/contact', data=data)
+
+    def test_contact_form(self):
+        subject = 'subject'
+        reply_to = 'a@example.com'
+        message_body = 'body'
+        sender = lso.app.config['DEFAULT_MAIL_SENDER']
+
+        with lso.mail.record_messages() as outbox:
+            # subject, email, and message
+            result = self.contact(dict(subject=subject,
+                                       email=reply_to,
+                                       message=message_body))
+
+            self.assertEqual(len(outbox), 1)
+            message = outbox[0]
+            self.assertEqual(message.sender, sender)
+            self.assertEqual(message.subject, subject)
+            self.assertEqual(message.reply_to, reply_to)
+            self.assertEqual(message.body, message_body)
+
+            # invalid email
+            result = self.contact(dict(subject=subject,
+                                       email='aoeu',
+                                       message=message_body))
+
+            self.assertEqual(len(outbox), 1)
+
+            # subject and message
+            result = self.contact(dict(subject=subject,
+                                       message=message_body))
+
+            self.assertEqual(len(outbox), 2)
+            message = outbox[1]
+            self.assertEqual(message.sender, sender)
+            self.assertEqual(message.subject, subject)
+            self.assertEqual(message.reply_to, None)
+            self.assertEqual(message.body, message_body)
+
+            # just subject
+            result = self.contact(dict(subject=subject))
+            self.assertEqual(len(outbox), 2)
+
+            # just message
+            result = self.contact(dict(message=message_body))
+            self.assertEqual(len(outbox), 2)
 
 if __name__ == '__main__':
     unittest.main()
