5bcacf356fdf84c3da7a9906cc7f6267c0b55f19
Arun Prasad <akprasad@stanford.edu>
2012-11-27 01:28:40 -0800

    Add: remove session on teardown request
    This frees up some memory and might resolve a lingering 500 error
    in production.

--- lso/views.py
+++ lso/views.py
@@ -1,9 +1,18 @@
 from flask import render_template
+
 from lso import app
+from lso.database import session
+
+
+@app.teardown_request
+def remove_session(exception=None):
+    session.remove()
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.errorhandler(404)
 def page_not_found(error):
