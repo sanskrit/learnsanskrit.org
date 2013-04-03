dd20c41b21b94764702897aa42cb16d645a9ef5e
Arun Prasad <akprasad@stanford.edu>
2013-04-03 16:35:12 -0700

    Add 500 handler

--- lso/views.py
+++ lso/views.py
@@ -17,3 +17,8 @@ def index():
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('errors/404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template('errors/500.html'), 500
