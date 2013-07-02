76c9d95b94647092f0f5c2a7a82478ea189c3797
Arun Prasad <akprasad@stanford.edu>
2013-07-01 22:25:34 -0700

    Fix tooltip behavior on edge of viewport

--- lso/static/js/tooltips.js
+++ lso/static/js/tooltips.js
@@ -2,14 +2,18 @@
  * An extremely lightweight tooltip plugin.
  */
 $.tooltips = function(selector, scope) {
-    // Bind to page if no scope provided
-    scope = scope || 'body';
     // Bind to titled elements if no selector provided
     selector = selector || '[title]';
-    var fadeSpeed = 100;
+    // Bind to page if no scope provided
+    scope = scope || 'body';
+    // Speed for fading in and out
+    var fadeSpeed = 100,
+    // Offset from cursor
+        offset = 5;
 
     $(scope).on({
         mouseover: function() {
+            console.log('over');
             var $this = $(this),
                 title = $this.data('title');
 
@@ -24,22 +28,36 @@ $.tooltips = function(selector, scope) {
 
             // Each matched element has access to its own tooltip. This
             // provides reasonable behavior in the (rare) case that two
-            // toolselector are active at once.
+            // tooltips are active at once.
             var $tt = $('<div class="tt" />').html(title)
                 .hide()
                 .appendTo('body')
                 .fadeIn(fadeSpeed);
             $this.data('tooltip', $tt);
+            console.log($tt);
         },
         mousemove: function(e) {
-            $(this).data('tooltip').css({
-                top: e.pageY + 6,
-                left: e.pageX + 6
-            });
+            var $tt = $(this).data('tooltip'),
+                el = $tt[0],
+                rect = el.getBoundingClientRect();
+
+            var top = e.pageY + offset,
+                left = e.pageX + offset;
+
+            var xOverlap = left + rect.width - $(window).width();
+            var yOverlap = top + rect.height - $(window).height();
+            if (xOverlap > 0) {
+                left = e.pageX - rect.width - offset;
+            }
+            if (yOverlap > 0) {
+                top = e.pageY - rect.height - offset;
+            }
+
+            $tt.css({ top: top, left: left });
         },
         mouseout: function() {
             $(this).data('tooltip').fadeOut(fadeSpeed, function() {
-                $(this).remove();
+                // $(this).remove();
             });
         }
     }, selector);
