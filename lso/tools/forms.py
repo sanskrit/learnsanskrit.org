9754a09c9d5b2646fe8ef9789529cc937f3981c0
Arun Prasad <akprasad.public@gmail.com>
2014-03-09 17:10:44 -0700

    Add tool: meter recognizer
    This is the very first version. It's pretty crude.

--- lso/tools/forms.py
+++ lso/tools/forms.py
@@ -2,6 +2,11 @@ from flask.ext.wtf import TextAreaField
 
 from lso.forms import SanskritForm
 
+
 class SanscriptForm(SanskritForm):
     input = TextAreaField()
     output = TextAreaField()
+
+
+class MeterForm(SanskritForm):
+    input = TextAreaField()
--- lso/tools/views.py
+++ lso/tools/views.py
@@ -1,17 +1,29 @@
-from flask import Blueprint, render_template
+from chandas import Classifier
+from flask import render_template
 
 from lso.lib import LSOBlueprint
 import forms
 
 bp = LSOBlueprint('tools', __name__, url_prefix='/tools')
-
+classifier = Classifier.from_json_file('/home/akp/projects/chandas/data/data.json')
 
 @bp.route('/')
 def index():
     return render_template('tools/index.html')
 
 
+@bp.route('/meter', methods=['GET', 'POST'])
+def meter():
+    """Meter recognizer."""
+    form = forms.MeterForm()
+    if form.validate_on_submit():
+        result = classifier.classify(form.input.data)
+        return render_template('tools/meter.html', form=form, result=result)
+    return render_template('tools/meter.html', form=form)
+
+
 @bp.route('/sanscript')
 def sanscript():
+    """Transliterator."""
     form = forms.SanscriptForm()
     return render_template('tools/sanscript.html', form=form)
