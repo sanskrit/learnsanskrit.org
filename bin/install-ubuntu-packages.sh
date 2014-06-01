55f4e5a7a13878551088c07419391207e7182eed
Arun Prasad <akprasad.public@gmail.com>
2014-06-01 16:35:04 -0700

    Add a single script to install Ubuntu packages

---
+++ bin/install-ubuntu-packages.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Python necessities
+sudo apt-get install python-dev python-pip
+
+# weasyprint
+sudo apt-get install python-lxml libffi-dev
+
+# Postgres
+sudo apt-get install postgresql postgresql-contrib
+
+# Less
+sudo apt-get install npm
+sudo npm install -g less
