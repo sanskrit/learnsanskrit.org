4b73f6a3196c60d1de418d44d235f8f9f44e9cfb
Arun Prasad <akprasad.public@gmail.com>
2015-02-06 23:41:02 -0800

    Add more tests for MW dictionary

--- test/dicts/test_views.py
+++ test/dicts/test_views.py
@@ -1,3 +1,4 @@
+# -*- encoding: utf-8 -*-
 import json
 
 import pytest
@@ -10,9 +11,36 @@ def mw_data(app):
     setup.run(app=app)
 
 
+def test_main_single_word_with_entries(mw_data, test_app):
+    results = test_app.get('/dicts/mw/q-hk/atha').data
+    assert 'an auspicious and inceptive particle' in results
+
+
+def test_main_single_word_no_entries(mw_data, test_app):
+    results = test_app.get('/dicts/mw/q-hk/garbage').data
+    assert 'No results found' in results
+
+
+def test_main_multiple_words(mw_data, test_app):
+    results = test_app.get('/api/mw/nara+aTa').data
+    assert 'an auspicious and inceptive particle' in results
+    assert 'a man, a male, a person' in results
+
+
+def test_main_empty_word(mw_data, test_app):
+    results = test_app.get('/api/mw/nara++aTa').data
+    assert 'an auspicious and inceptive particle' in results
+    assert 'a man, a male, a person' in results
+
+
+def test_main_single_word_devanagari(mw_data, test_app):
+    results = test_app.get('/dicts/mw/q-devanagari/अथ').data
+    assert 'an auspicious and inceptive particle' in results
+
+
 def test_api_single_word_with_entries(mw_data, test_app):
-    results = json.loads(test_app.get('/api/mw/aham').data)
-    assert results['aham']
+    results = json.loads(test_app.get('/api/mw/nara').data)
+    assert results['nara']
 
 
 def test_api_single_word_no_entries(mw_data, test_app):
@@ -21,12 +49,15 @@ def test_api_single_word_no_entries(mw_data, test_app):
 
 
 def test_api_multiple_words(mw_data, test_app):
-    results = json.loads(test_app.get('/api/mw/aham+aTa').data)
-    assert results['aham']
+    results = json.loads(test_app.get('/api/mw/nara+aTa').data)
+    assert results['nara']
     assert results['aTa']
 
 
 def test_api_empty_word(mw_data, test_app):
-    results = json.loads(test_app.get('/api/mw/aham++aTa').data)
-    assert results['aham']
+    results = json.loads(test_app.get('/api/mw/nara++aTa').data)
+    assert results['nara']
     assert results['aTa']
+
+
+# API queries are SLP1 only -> no tests for Devanagari
