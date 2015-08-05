d967dfe0f6418499264c3a63418305b332266e72
Arun Prasad <akprasad.public@gmail.com>
2015-08-04 20:33:31 -0700

    Add fab command for data deployment
    This contains the MW dictionary, among others.

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,5 @@
 from fabric.api import *
-from fabric.contrib.project import rsync_project
+from fabric.contrib.project import rsync_project, upload_project
 import production
 
 env.hosts = [production.host]
@@ -9,3 +9,17 @@ def deploy_static():
     # without trailing slash, files are moved to `remote_dir`/static.
     rsync_project(local_dir='lso/static/',
                   remote_dir=production.static_remote_dir, delete=True)
+
+
+def deploy_data():
+    run('mkdir -p {}'.format(production.data_dir))
+    run('mkdir -p {}'.format(production.monier_dir))
+
+    # Data will be in `data_dir`/all-data
+    # `upload_data` uses gzip, and these files are large.
+    upload_project(local_dir='~/projects/sanskrit-data/all-data',
+                  remote_dir=production.data_dir)
+
+    # Data will be in `data_dir`/monier-williams
+    upload_project(local_dir='~/projects/sanskrit-data/monier-williams',
+                  remote_dir=production.monier_dir)
