0cfbdefae4c17365cbb8e72a32689dce857c7d93
Arun Prasad <akprasad.public@gmail.com>
2015-08-04 22:15:33 -0700

    Remove circular dep on `production` in index.py
    Otherwise we have a circular dependency.

--- index.py
+++ index.py
@@ -1,11 +1,11 @@
 import os
 import sys
 
+sys.path.append(os.path.dirname(__file__))
 import production
 
-activate_this = os.path.join(production.virtualenv_path, '/bin/activate_this.py')
+activate_this = os.path.join(production.virtualenv_path, 'bin/activate_this.py')
 execfile(activate_this, dict(__file__=activate_this))
-sys.path.append(production.htdocs_dir)
 
 from lso import create_app
 application = create_app(__name__, 'production')
