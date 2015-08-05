2e470aa0f59dff35ac6a7a8538592e199afa5207
Arun Prasad <akprasad.public@gmail.com>
2015-08-04 21:20:20 -0700

    Switch fabfile uploads to use rsync
    The initial upload is slower, but all changes after are reasonably fast
    and painless.

--- fabfile.py
+++ fabfile.py
@@ -1,25 +1,28 @@
 from fabric.api import *
-from fabric.contrib.project import rsync_project, upload_project
+from fabric.contrib.project import rsync_project
 import production
 
 env.hosts = [production.host]
 
 
 def deploy_static():
+    """Deploy static assets (CSS, JS, ...)"""
     # without trailing slash, files are moved to `remote_dir`/static.
     rsync_project(local_dir='lso/static/',
                   remote_dir=production.static_remote_dir, delete=True)
 
 
 def deploy_data():
-    run('mkdir -p {}'.format(production.data_dir))
-    run('mkdir -p {}'.format(production.monier_dir))
-
+    """Deploy large data files (MW dictionary, linguistic data)"""
     # Data will be in `data_dir`/all-data
-    # `upload_data` uses gzip, and these files are large.
-    upload_project(local_dir='~/projects/sanskrit-data/all-data',
+    rsync_project(local_dir='~/projects/sanskrit-data/all-data',
                   remote_dir=production.data_dir)
 
     # Data will be in `data_dir`/monier-williams
-    upload_project(local_dir='~/projects/sanskrit-data/monier-williams',
+    rsync_project(local_dir='~/projects/sanskrit-data/monier-williams',
                   remote_dir=production.monier_dir)
+
+
+def deploy_all():
+    deploy_static()
+    deploy_data()
